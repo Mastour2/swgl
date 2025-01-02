271716bc0246973a830c82f0794f0816b1360524
Mastour <49590405+Mastour2@users.noreply.github.com>
2025-01-02 06:11:24 +0300

    Update shader.js

--- src/shader.js
+++ src/shader.js
@@ -2,40 +2,27 @@ const data = new Float32Array([
     [-0.5, 0.5],
     [-0.5, -0.5],
     [0.5, -0.5],
-    [0.5, 0.5],
 ].flat())
 
 
 const vertex = `#version 300 es
 layout(location = 0) in vec2 aPos;
-layout(location = 1) in vec2 aTexCoord;
-
-out vec2 vTexCoord;
 
 void main() {
-    vTexCoord = aTexCoord;
     gl_Position = vec4(aPos, 0, 1);
     gl_PointSize = 2.0;
 }
 `
 
-
 const fragment = `#version 300 es
 precision mediump float;
-
-uniform sampler2D uTex;
-
-in vec2 vTexCoord;
 
 out vec4 fragColor;
 
 void main() {
-    vec2 adjust = vec2(0.5 + vTexCoord.x, 0.5 - vTexCoord.y);
-    fragColor = texture(uTex, adjust);
+    fragColor = vec4(1);
 }
-
 `
-
 
 export {
     vertex, fragment, data
